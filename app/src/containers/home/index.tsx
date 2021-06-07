b5576e3da0f71c18c6bfb8efb325a468953ab684
hieulai <>
2021-06-07 16:36:35 +1000

    add contributor section

--- app/src/containers/home/index.tsx
+++ app/src/containers/home/index.tsx
@@ -1,24 +1,27 @@
-import React from "react";
-import cx from "classnames";
-import { css } from "@emotion/css";
-import { ThemeContext } from "../../contexts/ThemeContext";
-import Tabs from "./tabs";
-import Header from "./header";
+import { css } from '@emotion/css'
+import cx from 'classnames'
+import React from 'react'
 
-
+import { ThemeContext } from '../../contexts/ThemeContext'
+import Contributors from './contributors'
+import Header from './header'
+import Tabs from './tabs'
 
 export default function Home() {
-  const [theme] = React.useContext(ThemeContext);
+  const [theme] = React.useContext(ThemeContext)
 
   return (
-    <div className={cx(css`
-      color: ${theme.color.white};
-      display: flex;
-      align-items: center;
-      flex-direction: column;
-    `)}>
+    <div
+      className={cx(css`
+        color: ${theme.color.white};
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+      `)}
+    >
       <Header />
       <Tabs />
+      <Contributors />
     </div>
-  );
+  )
 }
