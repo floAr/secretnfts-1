74585ebfffa485db4039acec337b73f1136235b5
hieulai <>
2021-06-09 11:48:26 +1000

    📱 add responsive design

--- app/src/components/contributor.tsx
+++ app/src/components/contributor.tsx
@@ -24,24 +24,33 @@ const ContributorCard: FC<Props> = ({ name, image, position, socials }) => {
     <div
       className={cx(css`
         align-items: center;
+        aspect-ratio: 0.7;
         background: #fff;
         border: 1px solid ${theme.color.greylight};
-        border-radius: ${theme.borderRadii.medium}px;
+        border-radius: ${theme.borderRadius.medium}px;
         box-sizing: border-box;
         box-shadow: 1px 4px 6px 6px ${theme.color.grey}0f;
         display: flex;
         flex-direction: column;
-        height: 396px;
         padding: ${theme.spacing.medium}px;
-        width: 276px;
+        width: 275px;
+
+        @media only screen and (max-width: ${theme.breakpoint.tablet}px) {
+          padding: ${theme.spacing.small}px;
+          width: 200px;
+        }
+
+        @media only screen and (max-width: ${theme.breakpoint.mobileM}px) {
+          padding: ${theme.spacing.xsmall}px;
+          width: 170px;
+        }
       `)}
     >
-      <div
+      <img
+        src={image}
         className={cx(css`
-          background: ${theme.color.greylight};
           border-radius: 50%;
-          height: 200px;
-          width: 200px;
+          width: 85%;
         `)}
       />
       <h3
@@ -49,6 +58,15 @@ const ContributorCard: FC<Props> = ({ name, image, position, socials }) => {
           color: ${theme.color.blacklight};
           font-size: 24px;
           font-weight: 500;
+
+          @media only screen and (max-width: ${theme.breakpoint.tablet}px) {
+            font-size: 16px;
+          }
+
+          @media only screen and (max-width: ${theme.breakpoint.mobileM}px) {
+            margin: ${theme.spacing.small}px 0;
+            font-size: 14px;
+          }
         `)}
       >
         {name}
@@ -60,18 +78,37 @@ const ContributorCard: FC<Props> = ({ name, image, position, socials }) => {
           font-weight: 400;
           letter-spacing: 0.15px;
           margin: 0;
+
+          @media only screen and (max-width: ${theme.breakpoint.tablet}px) {
+            font-size: 14px;
+          }
+
+          @media only screen and (max-width: ${theme.breakpoint.mobileM}px) {
+            font-size: 12px;
+          }
         `)}
       >
         {position}
       </p>
       <div
         className={cx(css`
+          /* border: 1px solid red; */
           align-items: center;
           column-gap: ${theme.spacing.medium}px;
           display: flex;
           justify-content: center;
           margin-top: 24px;
           width: 100%;
+
+          @media only screen and (max-width: ${theme.breakpoint.tablet}px) {
+            column-gap: ${theme.spacing.small}px;
+            margin-top: 15px;
+          }
+
+          @media only screen and (max-width: ${theme.breakpoint.mobileM}px) {
+            column-gap: ${theme.spacing.xsmall}px;
+            margin-top: 10px;
+          }
         `)}
       >
         {socials.map(({ link, icon }) => (
