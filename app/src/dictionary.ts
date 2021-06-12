de0a1631cfd23009018490d18d16268d6dd58f4a
hieulai <>
2021-06-12 15:59:52 +1000

    📝 add translator for footer / subscribe

--- app/src/dictionary.ts
+++ app/src/dictionary.ts
@@ -1,188 +1,235 @@
 interface dictionary {
-    [name: string]: any
+  [name: string]: any
 }
 
 export const dictionary: dictionary = {
-    WHY_NFT: {
-        en: "Why Privacy Matters for NFTs?",
-        pt: ""
-    },
-    BUY_SCRT: {
-        en: "Buy SCRT",
-        pt: ""
-    },
-    ART_COLLECTIBLES_TITLE: {
-        en: "Art & Collectibles",
-        pt: ""
-    },
-    PRIVATE_GALLERIES_TITLE: {
-        en: `Private Galleries & Ownership`,
-        pt: ""
-    },
-    PRIVATE_GALLERIES_INFO: {
-        en: `Art galleries can restrict viewership to maintain
+  WHY_NFT: {
+    en: 'Why Privacy Matters for NFTs?',
+    pt: '',
+  },
+  BUY_SCRT: {
+    en: 'Buy SCRT',
+    pt: '',
+  },
+  ART_COLLECTIBLES_TITLE: {
+    en: 'Art & Collectibles',
+    pt: '',
+  },
+  PRIVATE_GALLERIES_TITLE: {
+    en: `Private Galleries & Ownership`,
+    pt: '',
+  },
+  PRIVATE_GALLERIES_INFO: {
+    en: `Art galleries can restrict viewership to maintain
         rarity and exclusivity and art owners can keep
         their ownership status private`,
-        pt: ""
-    },
-    FINANCIAL_PRIVACY_TITLE: {
-        en: `Financial Privacy for Artists`,
-        pt: ""
-    },
-    FINANCIAL_PRIVACY_INFO: {
-        en: `Artists and collectors can maintain financial
+    pt: '',
+  },
+  FINANCIAL_PRIVACY_TITLE: {
+    en: `Financial Privacy for Artists`,
+    pt: '',
+  },
+  FINANCIAL_PRIVACY_INFO: {
+    en: `Artists and collectors can maintain financial
         privacy by decoupling wallet balances from
         NFT art and collectibles`,
-        pt: ""
-    },
-    SEALED_AUCTIONS_TITLE: {
-        en: `Sealed Bid Auctions`,
-        pt: ""
-    },
-    SEALED_AUCTIONS_INFO: {
-        en: `Sealed bid auctions keep a collector's wallet
+    pt: '',
+  },
+  SEALED_AUCTIONS_TITLE: {
+    en: `Sealed Bid Auctions`,
+    pt: '',
+  },
+  SEALED_AUCTIONS_INFO: {
+    en: `Sealed bid auctions keep a collector's wallet
         confidential, so that that their balance cannot be
         used against them during bidding`,
-        pt: ""
-    },
+    pt: '',
+  },
 
-    DIGITAL_MEDIA_TITLE: {
-        en: "Digital Media",
-        pt: ""
-    },
-    EXCLUSIVE_CONTENT_TITLE: {
-        en: `Exclusive Content & Paywalls`,
-        pt: ""
-    },
-    EXCLUSIVE_CONTENT_INFO: {
-        en: `Privileged content shared only with customers
+  DIGITAL_MEDIA_TITLE: {
+    en: 'Digital Media',
+    pt: '',
+  },
+  EXCLUSIVE_CONTENT_TITLE: {
+    en: `Exclusive Content & Paywalls`,
+    pt: '',
+  },
+  EXCLUSIVE_CONTENT_INFO: {
+    en: `Privileged content shared only with customers
         granted access, based on community ranking, user
         engagement, or payment eligibility`,
-        pt: ""
-    },
-    WATERMARKET_TITLE: {
-        en: `Watermarked Content`,
-        pt: ""
-    },
-    WATERMARKET_INFO: {
-        en: `Wiatermarked content display samples or sneak
+    pt: '',
+  },
+  WATERMARKET_TITLE: {
+    en: `Watermarked Content`,
+    pt: '',
+  },
+  WATERMARKET_INFO: {
+    en: `Wiatermarked content display samples or sneak
         peaks of protected content without fully revealing
         an artist's work in a decentralized manner`,
-        pt: ""
-    },
-    ADULT_CONTENT_TITLE: {
-        en: `Adult Content`,
-        pt: ""
-    },
-    ADULT_CONTENT_INFO: {
-        en: `Sensitive content censored by creators to restrict
+    pt: '',
+  },
+  ADULT_CONTENT_TITLE: {
+    en: `Adult Content`,
+    pt: '',
+  },
+  ADULT_CONTENT_INFO: {
+    en: `Sensitive content censored by creators to restrict
         viewership to fans granted selective access or meet
         certain age restrictions`,
-        pt: ""
-    },
+    pt: '',
+  },
 
-    GAMING_TITLE: {
-        en: "Gaming Industry",
-        pt: ""
-    },
-    HIDDEN_LOOT_TITLE: {
-        en: `Hidden Loot Boxes & Card Packs`,
-        pt: ""
-    },
-    HIDDEN_LOOT_INFO: {
-        en: `Loot boxes and card packs with mystery or rare
+  GAMING_TITLE: {
+    en: 'Gaming Industry',
+    pt: '',
+  },
+  HIDDEN_LOOT_TITLE: {
+    en: `Hidden Loot Boxes & Card Packs`,
+    pt: '',
+  },
+  HIDDEN_LOOT_INFO: {
+    en: `Loot boxes and card packs with mystery or rare
         content will only be revealed after the owner
         unlocks the content`,
-        pt: ""
-    },
-    PLAYABLE_TITLE: {
-        en: `Playable Trading Games`,
-        pt: ""
-    },
-    PLAYABLE_INFO: {
-        en: `Games relying on secrecy to hide player actions
+    pt: '',
+  },
+  PLAYABLE_TITLE: {
+    en: `Playable Trading Games`,
+    pt: '',
+  },
+  PLAYABLE_INFO: {
+    en: `Games relying on secrecy to hide player actions
         from other players, unlock card games like Pokemon,
         Yugioh, and Magic`,
-        pt: ""
-    },
-    SKILLS_CRAFTING_TITLE: {
-        en: `Skills & Crafting`,
-        pt: ""
-    },
-    SKILLS_CRAFTING_INFO: {
-        en: `Player avatars can unlock hidden skills and
+    pt: '',
+  },
+  SKILLS_CRAFTING_TITLE: {
+    en: `Skills & Crafting`,
+    pt: '',
+  },
+  SKILLS_CRAFTING_INFO: {
+    en: `Player avatars can unlock hidden skills and
         exclusive content after a set period time or
         extended user activity`,
-        pt: ""
-    },
+    pt: '',
+  },
 
-    REAL_LIFE_TITLE: {
-        en: "Real-Life Application",
-        pt: ""
-    },
-    IDS_TITLE: {
-        en: `Identification Cards & Passports`,
-        pt: ""
-    },
-    IDS_INFO: {
-        en: `Identification cards and passports linked to
+  REAL_LIFE_TITLE: {
+    en: 'Real-Life Application',
+    pt: '',
+  },
+  IDS_TITLE: {
+    en: `Identification Cards & Passports`,
+    pt: '',
+  },
+  IDS_INFO: {
+    en: `Identification cards and passports linked to
         personal information should be kept private for user
         safety`,
-        pt: ""
-    },
-    LUXURY_TITLE: {
-        en: `Property & Luxury Goods`,
-        pt: ""
-    },
-    LUXURY_INFO: {
-        en: `Real estate and luxury goods that require transfer
+    pt: '',
+  },
+  LUXURY_TITLE: {
+    en: `Property & Luxury Goods`,
+    pt: '',
+  },
+  LUXURY_INFO: {
+    en: `Real estate and luxury goods that require transfer
         history for ownership can store person information
         as private metadata`,
-        pt: ""
-    },
-    TICKETS_TITLE: {
-        en: `Events & Ticketing`,
-        pt: ""
-    },
-    TICKETS_INFO: {
-        en: `Tickets and event tokens can be created that still
+    pt: '',
+  },
+  TICKETS_TITLE: {
+    en: `Events & Ticketing`,
+    pt: '',
+  },
+  TICKETS_INFO: {
+    en: `Tickets and event tokens can be created that still
         protect private metadata associating a person to a
         specific location`,
-        pt: ""
-    },
+    pt: '',
+  },
 
-    STEP0: {
-        en: `Step 1`,
-        pt: ""
-    },
-    STEP1: {
-        en: `Step 2`,
-        pt: ""
-    },
-    STEP2: {
-        en: `Step 3`,
-        pt: ""
-    },
-    STEP3: {
-        en: `Step 4`,
-        pt: ""
-    },
+  STEP0: {
+    en: `Step 1`,
+    pt: '',
+  },
+  STEP1: {
+    en: `Step 2`,
+    pt: '',
+  },
+  STEP2: {
+    en: `Step 3`,
+    pt: '',
+  },
+  STEP3: {
+    en: `Step 4`,
+    pt: '',
+  },
 
-    STEP0_TEXT: {
-        en: `Create a collection for your nfts`,
-        pt: ""
-    },
+  STEP0_TEXT: {
+    en: `Create a collection for your nfts`,
+    pt: '',
+  },
 
-    STEP1_TEXT: {
-        en: `Upload a media file to your NFT`,
-        pt: ""
-    },
-    STEP2_TEXT: {
-        en: `Name, provide a description and add properties to your NFT`,
-        pt: ""
-    },
-    STEP3_TEXT: {
-        en: `*Optional, Protect your NFT`,
-        pt: ""
-    },
+  STEP1_TEXT: {
+    en: `Upload a media file to your NFT`,
+    pt: '',
+  },
+  STEP2_TEXT: {
+    en: `Name, provide a description and add properties to your NFT`,
+    pt: '',
+  },
+  STEP3_TEXT: {
+    en: `*Optional, Protect your NFT`,
+    pt: '',
+  },
+
+  SUBSCRIBE_TITLE: {
+    en: `Find out more 🎉`,
+    pt: '',
+  },
+  SUBSCRIBE_TEXT: {
+    en: `Subscribe to the newsletter to hear about Secret NFTs updates and
+        events.`,
+    pt: '',
+  },
+  SUBSCRIBE_BUTTON_TEXT: {
+    en: `Discover SNFTs`,
+    pt: '',
+  },
+
+  FOOTER_TAGLINE: {
+    en: 'The most secured decentralised NFT in cryptospace.',
+    pt: '',
+  },
+  FOOTER_MENU_ONE_TITLE: {
+    en: 'Secret Network',
+    pt: '',
+  },
+  FOOTER_MENU_ONE_TAB_ONE: {
+    en: 'Discover',
+    pt: '',
+  },
+  FOOTER_MENU_ONE_TAB_TWO: {
+    en: 'How to buy SCRT',
+    pt: '',
+  },
+  FOOTER_MENU_ONE_TAB_THREE: {
+    en: 'News & Updates',
+    pt: '',
+  },
+  FOOTER_MENU_TWO_TITLE: {
+    en: 'Support',
+    pt: '',
+  },
+  FOOTER_MENU_TWO_TAB_ONE: {
+    en: 'How it works',
+    pt: '',
+  },
+  FOOTER_MENU_TWO_TAB_TWO: {
+    en: 'Request a feature',
+    pt: '',
+  },
 }
