2902e36abd8cc35076aa415dcba72eda0f83f448
Florian Uhde <floar@mx.de>
2021-06-15 14:39:49 +0200

    work on deployment

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-global.fetch = require('node-fetch')
+global.fetch = require("node-fetch");
 
 const helmet = require("helmet");
 const rateLimit = require("express-rate-limit");
@@ -14,18 +14,26 @@ const { secretConnection } = require("./secret");
 
 const app = express();
 const limiter = rateLimit({ windowMs: 1000, max: 65 });
-app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
 app.use(limiter);
 app.use(cors());
+// app.use(csp({
+//     directives: {
+//       defaultSrc: [`'self'`],
+//     }
+//   }))
 const server = http.createServer(app);
 
 const io = socketio(server, {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"]
-    }
+  cors: {
+    origin: ["http://localhost:3000","http://localhost:8080","https://secretnfts.azurewebsites.net/"],
+    methods: ["GET", "POST"],
+  },
 });
-
 
 const clients = new Map();
 let scrtprice = [];
@@ -33,84 +41,88 @@ global.databse = null;
 global.dbcollections = null;
 global.secretjs = null;
 
-console.log('db', process.env.MONGODB_URL)
+console.log("db", process.env.MONGO_URL);
 mongo.connect(
-    process.env.MONGODB_URL,
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    async (err, client) => {
-        if (err) return console.error(err);
-        const database = client.db("secretnfts");
-        global.database = database;
-        global.dbcollections = database.collection("collections");
-        global.secretjs = await secretConnection();
-        console.log('global.secretjs', global.secretjs)
-        //global.dbcollections.remove({}, function (err, removed) { });
+  process.env.MONGODB_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  async (err, client) => {
+    if (err) return console.error(err);
+    const database = client.db("secretnfts");
+    global.database = database;
+    global.dbcollections = database.collection("collections");
+    global.secretjs = await secretConnection();
+    console.log("global.secretjs", global.secretjs);
+    //global.dbcollections.remove({}, function (err, removed) { });
 
-        fetchSCRTPrice((prices) => {
-            scrtprice = prices;
-        });
-    }
+    fetchSCRTPrice((prices) => {
+      scrtprice = prices;
+    });
+  }
 );
 
-
-
-
 io.on("connection", (socket) => {
-    clients.set(socket.id, { socket, timestamp: new Date().getTime(), id: socket.id });
+  clients.set(socket.id, {
+    socket,
+    timestamp: new Date().getTime(),
+    id: socket.id,
+  });
 
-    socket.on("handshake", (callback) => {
-        console.log(`${socket.id} is connecting`);
-        const response = {
-            scrtprice
-        };
-        callback(response);
+  socket.on("handshake", (callback) => {
+    console.log(`${socket.id} is connecting`);
+    const response = {
+      scrtprice,
+    };
+    callback(response);
+  });
+
+  socket.on("getCollection", (address, callback) => {
+    global.dbcollections.findOne({ address: address }, (err, result) => {
+      callback(result);
     });
+  });
 
-    socket.on("getCollection", (address, callback) => {
-        global.dbcollections.findOne({ "address": address }, (err, result) => {
-            callback(result)
-        });
+  socket.on("getCollections", (params, callback) => {
+    const query = params.from ? { from: params.from } : {};
+    const limit = params.limit || 10;
+    const ascending = params.ascending ? 1 : -1;
+
+    global.dbcollections
+      .find(query)
+      .sort({ _id: ascending })
+      .limit(limit)
+      .toArray(function(err, result) {
+        callback(result || []);
+      });
+  });
+
+  socket.on("createCollection", (name, symbol, address, from, callback) => {
+    const collection = {
+      name,
+      symbol,
+      address,
+      from,
+    };
+
+    global.dbcollections.insertOne(collection, (err) => {
+      callback({ error: false, collection });
     });
+  });
 
-    socket.on("getCollections", (params, callback) => {
-        const query = params.from ? { "from": params.from } : {}
-        const limit = params.limit || 10
-        const ascending = params.ascending ? 1 : -1
-
-        global.dbcollections.find(query).sort({ _id: ascending }).limit(limit).toArray(function (err, result) {
-            callback(result || [])
-        });
-    });
-
-
-    socket.on("createCollection", (name, symbol, address, from, callback) => {
-        const collection = {
-            name,
-            symbol,
-            address,
-            from
-        }
-
-        global.dbcollections.insertOne(collection, (err) => {
-            callback({ error: false, collection });
-        });
-
-    });
-
-    socket.on("disconnect", function () {
-        clients.delete(socket.id);
-    });
-
+  socket.on("disconnect", function() {
+    clients.delete(socket.id);
+  });
 });
 
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/*", function (req, res) {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 const port = process.env.PORT || 8080;
 server.listen(port, "0.0.0.0", (err) => {
-    if (err) { return console.log(err.stack); }
-    return console.log(`server is listening on ${port}`);
+  if (err) {
+    return console.log(err.stack);
+  }
+  return console.log(`server is listening on ${port}`);
 });
